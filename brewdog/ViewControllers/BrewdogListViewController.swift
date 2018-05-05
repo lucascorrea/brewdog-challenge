12cf75ac5e6b24402d0bceac8ef55946d44a651e
Lucas Correa <contato@lucascorrea.com>
2018-05-05 13:03:04 +0100

    Structure folder MVVM, List of beers layout initial

---
+++ brewdog/ViewControllers/BrewdogListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BrewdogListViewController.swift
+//  brewdog
+//
+//  Created by Lucas Correa on 05/05/2018.
+//  Copyright © 2018 SiriusCode. All rights reserved.
+//
+
+import UIKit
+
+class BrewdogListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension BrewdogListViewController: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: BeerCell = tableView.dequeueReusableCell(withIdentifier: "BeerCell", for: indexPath) as! BeerCell
+//        stationsViewModel.configureCell(cell: cell, indexPath: indexPath)
+        return cell
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension BrewdogListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 240.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
---
+++ brewdog/Views/BeerCell.swift
@@ -0,0 +1,29 @@
+//
+//  BeerCell.swift
+//  brewdog
+//
+//  Created by Lucas Correa on 05/05/2018.
+//  Copyright © 2018 SiriusCode. All rights reserved.
+//
+
+import UIKit
+
+class BeerCell: UITableViewCell {
+
+    @IBOutlet weak var beerImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var abvLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
